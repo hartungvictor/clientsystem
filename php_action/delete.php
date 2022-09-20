c1fd2377993807700f5fa40a5d5830671d4af38b
hartungvictor <victor_hartung@hotmail.com>
2022-09-20 15:40:58 -0300

    first commit

---
+++ php_action/delete.php
@@ -0,0 +1,24 @@
+<?php
+//Sessão
+session_start();
+
+//Conexão
+require_once 'db_connect.php';
+
+if(isset($_POST['btn-deletar'])){
+    
+    $id = mysqli_escape_string($connect, $_POST['id']);
+
+    $sql ="DELETE FROM clientes WHERE id = '$id'";
+
+    if(mysqli_query($connect, $sql)){
+        $_SESSION['mensagem'] = "Deletado com sucesso!";
+        Header('Location: ../index.php?sucesso');
+    }else{
+        $_SESSION['mensagem'] = "Erro ao deletar!";
+        Header('Location: .../index.php?erro');
+    }
+}
+
+
+?>
